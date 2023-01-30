c1d741df93d8847de1aea7d3f8cec2c9df2786b6
Sebastopaul <sebastien.lamard@epitech.eu>
2023-01-31 00:12:46 +0100

    feat: Adding doc-blocs to Time.hpp

--- KappaEngine/Time.hpp
+++ KappaEngine/Time.hpp
@@ -9,21 +9,45 @@
 #include <functional>
 
 namespace KappaEngine {
+
+    /**
+     * @brief The Time class allows some parts of the program to run on its own environnement, with a fixed time.
+     */
     class Time {
         public:
+
+        /**
+         * @brief DeltaTime gets the amount of time per frame in seconds.
+         * 
+         * @return * sf::Time, the amount of time that a frame takes in seconds.
+         */
         static sf::Time DeltaTime() {
             return _deltaTime;
         };
 
+        /**
+         * @brief UpdateDeltaTime restarts the deltaClock and updates deltaTime with the time elapsed since it was last updated.
+         * 
+         * @return sf::Time, the time elapsed since the clock was last restarted.
+         */
         static sf::Time UpdateDeltaTime() {
             _deltaTime = _deltaClock.restart();
             return _deltaTime;
         };
 
+        /**
+         * @brief FixedDeltaTime is an attribute of the engine Time class that allows the systems to run their methods on a fixed amount of time.
+         * 
+         * @return double, the attribute _fixedDeltaTime.
+         */
         static double FixedDeltaTime() {
             return _fixedDeltaTime;
         };
 
+        /**
+         * @brief resetTimeLib resets all attributes of KappaEngine::Time.
+         * 
+         */
         static void resetTimeLib() {
             _deltaClock.restart();
             _deltaTime = sf::Time::Zero;
@@ -31,6 +55,13 @@ namespace KappaEngine {
             _lags = 0.0;
         }
 
+        /**
+         * @brief RunOnFixedEnv allows the systems to run a method several times to complete an action on a fixed amount of time.
+         * 
+         * For example, it can be used to handle the movement of a bullet or that of a jumping character.
+         * 
+         * @param func The function to be executed several times on a fixed amount of time.
+         */
         static void RunOnFixedEnv(const std::function<void()>& func) {
             _lags += _deltaTime.asSeconds();
             while (_lags >= _fixedDeltaTime) {
