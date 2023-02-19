68a0d11573c401699b2a059bf33e6b0af6095fc8
Léo Brossard <leo_brossard@hotmail.fr>
2023-02-19 22:49:23 +0100

    fix(network): updated system for network integration

--- src/Systems/RigidBodySystem.cpp
+++ src/Systems/RigidBodySystem.cpp
@@ -4,8 +4,11 @@
 
 #include <iostream>
 #include <ctime>
+
+#include "KappaEngine/GameManager.hpp"
 #include "KappaEngine/Systems/RigidBodySystem.hpp"
 #include "KappaEngine/Components/Transform.hpp"
+#include "KappaEngine/Components/NetworkComponent.hpp"
 #include "KappaEngine/Time.hpp"
 
 namespace KappaEngine {
@@ -17,6 +20,19 @@ namespace KappaEngine {
         auto ents = _scene->getEntityManager()->getEntitiesWithComponent<Component::RigidBody>();
 
         for (auto ent: ents) {
+            if (GameManager::isNetworked()) {
+                auto net = ent->getComponent<Component::NetworkComponent>();
+
+                if (GameManager::GetServer() && net && net->ownerId != -1) {
+                    continue;
+                }
+
+                if (GameManager::GetClient() && net && net->ownerId == GameManager::GetClient()->GetID()) {
+                    continue;
+                }
+            }
+
+
             auto rg = ent->getComponent<Component::RigidBody>();
             auto tr = ent->getComponent<Component::Transform>();
 
