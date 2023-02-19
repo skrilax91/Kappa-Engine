132f553e37bab01039877b124e400e9e049b57d1
DylanAkp <59843812+DylanAkp@users.noreply.github.com>
2023-02-19 16:56:03 +0100

    feat(Animator): fixed hpp with right declaration

--- include/KappaEngine/Components/Animation.hpp
+++ include/KappaEngine/Components/Animation.hpp
@@ -1,33 +1,33 @@
 #include <string>
 #include "IComponent.hpp"
+#include "SpriteRenderer.hpp"
+#include <list>
 
 namespace Component {
 
-    /**
-     * @brief The Animation class
-     * @def This class is used to create an animation
-     * @def The texturePath is the path to the texture
-     * @def The textureRect is the rectangle of the texture to use
-     * @def The layer is the layer to render the sprite
-     * @def The frameCount is the number of frames in the animation
-     * @def The frameDuration is the duration of each frame
-     * @def The loop is a boolean to know if the animation should loop
-     */
-    class Animation : public IComponent {
+    class Animation {
         public:
-            Animation(std::string texturePath, sf::IntRect textureRect, std::string layer, int frameCount, float frameDuration, bool loop):
+            Animation(std::string id, std::string texturePath, Component::SpriteRenderer & spriteRenderer, std::list<sf::IntRect> textureRect, int frameCount, float frameDuration, bool loop):
                 _texturePath(std::move(texturePath)),
                 _textureRect(textureRect),
-                _layer(std::move(layer)),
+                _spriteRenderer(spriteRenderer),
                 _frameCount(frameCount),
                 _frameDuration(frameDuration),
                 _loop(loop) {};
+                _id(id) {};
 
             std::string _texturePath;
+            Component::SpriteRenderer & _spriteRenderer;
             sf::IntRect _textureRect;
-            std::string _layer;
+            std::string _id;
             int _frameCount;
             float _frameDuration;
             bool _loop;
+    };
+    class Animator : public IComponent {
+        public:
+            Animator(std::list<Animation> animations, std::string idleAnimation):
+                _animations(animations),
+                _idleAnimation(std::move(idleAnimation)) {};
     };
 }
