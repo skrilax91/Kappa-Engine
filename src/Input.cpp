7b0fbbe2ca30804384676f9234e351ad21ea06ba
Léo Brossard <leo_brossard@hotmail.fr>
2023-02-19 16:36:22 +0100

    feat(networkKey): adding key networking

--- src/Input.cpp
+++ src/Input.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "KappaEngine/Input.hpp"
+#include "KappaEngine/GameManager.hpp"
+#include "KappaEngine/Network/MessageDefine.hpp"
 
 namespace KappaEngine {
     std::vector<const sf::Event *> Input::_events;
@@ -14,8 +16,26 @@ namespace KappaEngine {
         for (auto &event : _events) {
             if (event->type == sf::Event::KeyPressed) {
                 _keysPressed[event->key.code] = true;
+                if (GameManager::isNetworked() && GameManager::GetClient()) {
+
+                    Network::Message msg;
+                    msg.header.id = Network::NetworkMsg::InputPressed;
+
+                    msg << event->key.code;
+
+                    GameManager::GetClient()->Send(msg);
+                }
             } else if (event->type == sf::Event::KeyReleased) {
                 _keysPressed[event->key.code] = false;
+                if (GameManager::isNetworked() && GameManager::GetClient()) {
+
+                    Network::Message msg;
+                    msg.header.id = Network::NetworkMsg::InputReleased;
+
+                    msg << event->key.code;
+
+                    GameManager::GetClient()->Send(msg);
+                }
             }
         }
 
