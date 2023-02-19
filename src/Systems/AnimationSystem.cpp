3bb4099cf624cd8827f109ba7fc91919ec5e292f
DylanAkp <59843812+DylanAkp@users.noreply.github.com>
2023-02-19 16:56:03 +0100

    feat(Animator): fixed hpp with right declaration

--- src/Systems/AnimationSystem.cpp
+++ src/Systems/AnimationSystem.cpp
@@ -4,4 +4,12 @@ namespace KappaEngine {
     void AnimationSystem::Awake(std::shared_ptr<Entity> entity) {
         std::cout << "AnimationSystem Awake" << std::endl;
     }
+
+    void AnimationSystem::Update(std::shared_ptr<Entity> entity) {
+        std::cout << "AnimationSystem Update" << std::endl;
+    }
+
+    void AnimationSystem::FixedUpdate(std::shared_ptr<Entity> entity) {
+        std::cout << "AnimationSystem FixedUpdate" << std::endl;
+    }
 }
