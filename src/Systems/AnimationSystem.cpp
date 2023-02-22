7f6213db88db085a8059b5952fbc0db9b8475a01
DylanAkp <59843812+DylanAkp@users.noreply.github.com>
2023-02-22 13:52:03 +0100

    fix(animator): cpp updated

--- src/Systems/AnimationSystem.cpp
+++ src/Systems/AnimationSystem.cpp
@@ -7,19 +7,19 @@ namespace KappaEngine {
         for (auto entity : entities) {
             auto animator = entity->getComponent<Component::Animator>();
             auto animation = animator->_animations[animator->_actualAnimation];
-            if (animation._clock.getElapsedTime().asSeconds() >= animation._duration) {
-                animation._actualFrame++;
-                if (animation._actualFrame >= animation._textureRect.size()) {
-                    if (animation._loop) {
-                        animation._actualFrame = 0;
+            if (animation->_clock.getElapsedTime().asSeconds() >= animation->_duration) {
+                animation->_actualFrame++;
+                if (animation->_actualFrame >= animation->_textureRect.size()) {
+                    if (animation->_loop) {
+                        animation->_actualFrame = 0;
                     } else {
-                        animation._actualFrame--;
+                        animation->_actualFrame--;
                     }
                 }
-                animation._clock.restart();
+                animation->_clock.restart();
             }
-            animation._spriteRenderer._sprite.setTexture(animation._texture);
-            animation._spriteRenderer._sprite.setTextureRect(animation._textureRect[animation._actualFrame]);
+            animation->_spriteRenderer._sprite.setTexture(animation->_texture);
+            animation->_spriteRenderer._sprite.setTextureRect(animation->_textureRect[animation->_actualFrame]);
         }
     }
 }
