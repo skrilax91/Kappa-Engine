c12c713aa34e35e837a8a657ae626c7ab420e991
Léo Brossard <leo_brossard@hotmail.fr>
2023-03-04 21:54:14 +0100

    fix(input): fixed input results

--- src/Interfaces/InputInterface.cpp
+++ src/Interfaces/InputInterface.cpp
@@ -40,63 +40,126 @@ void Interface::InputInterface::OnRenderInterface(IPosition parent) {
     if (_isFocused) {
         _inputText.setFillColor(sf::Color::Black);
 
+        bool shiftPressed = sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift);
+        bool ctrlPressed = sf::Keyboard::isKeyPressed(sf::Keyboard::LControl) || sf::Keyboard::isKeyPressed(sf::Keyboard::RControl);
+        bool altPressed = sf::Keyboard::isKeyPressed(sf::Keyboard::LAlt) || sf::Keyboard::isKeyPressed(sf::Keyboard::RAlt);
+
         std::vector<sf::Keyboard::Key> keys = KappaEngine::Input::getKeysPressed();
         bool localPressed = false;
         for (auto &key : keys) {
+            if (key == sf::Keyboard::LShift || key == sf::Keyboard::RShift || key == sf::Keyboard::LControl || key == sf::Keyboard::RControl || key == sf::Keyboard::LAlt || key == sf::Keyboard::RAlt)
+                continue;
+
             localPressed = true;
+
             if (key >= sf::Keyboard::A && key <= sf::Keyboard::Z && !_pressed) {
-                _text += 'A' + key - sf::Keyboard::A;
+                if (shiftPressed)
+                    _text += 'A' + key - sf::Keyboard::A;
+                else
+                    _text += 'a' + key - sf::Keyboard::A;
                 _inputText.setString(_text);
             } else if (key >= sf::Keyboard::Num0 && key <= sf::Keyboard::Num9 && !_pressed) {
-                _text += '0' + key - sf::Keyboard::Num0;
+                switch (key) {
+                    case sf::Keyboard::Num1:
+                        if (shiftPressed)
+                            _text += '1';
+                        else
+                            _text += '&';
+                        break;
+                    case sf::Keyboard::Num2:
+                        if (shiftPressed)
+                            _text += "2";
+                        else if (altPressed)
+                            _text += '~';
+                        else
+                            _text += "é";
+                        break;
+                    case sf::Keyboard::Num3:
+                        if (shiftPressed)
+                            _text += "3";
+                        else if (altPressed)
+                            _text += '#';
+                        else
+                            _text += '"';
+                        break;
+                    case sf::Keyboard::Num4:
+                        if (shiftPressed)
+                            _text += "4";
+                        else if (altPressed)
+                            _text += '{';
+                        else
+                            _text += "'";
+                        break;
+                    case sf::Keyboard::Num5:
+                        if (shiftPressed)
+                            _text += "5";
+                        else if (altPressed)
+                            _text += '[';
+                        else
+                            _text += '(';
+                        break;
+                    case sf::Keyboard::Num6:
+                        if (shiftPressed)
+                            _text += "6";
+                        else if (altPressed)
+                            _text += '|';
+                        else
+                            _text += '-';
+                        break;
+                    case sf::Keyboard::Num7:
+                        if (shiftPressed)
+                            _text += "7";
+                        else if (altPressed)
+                            _text += '`';
+                        else
+                            _text += "è";
+                        break;
+                    case sf::Keyboard::Num8:
+                        if (shiftPressed)
+                            _text += "8";
+                        else if (altPressed)
+                            _text += '\\';
+                        else
+                            _text += "_";
+                        break;
+                    case sf::Keyboard::Num9:
+                        if (shiftPressed)
+                            _text += "9";
+                        else if (altPressed)
+                            _text += '^';
+                        else
+                            _text += "ç";
+                        break;
+                    case sf::Keyboard::Num0:
+                        if (shiftPressed)
+                            _text += "0";
+                        else if (altPressed)
+                            _text += '@';
+                        else
+                            _text += "à";
+                        break;
+                }
                 _inputText.setString(_text);
             } else if (key >= sf::Keyboard::Numpad0 && key <= sf::Keyboard::Numpad9 && !_pressed) {
                 _text += '0' + key - sf::Keyboard::Numpad0;
                 _inputText.setString(_text);
             } else if (key == sf::Keyboard::Period && !_pressed) {
-                _text += '.';
+                if (shiftPressed)
+                    _text += '.';
+                else
+                    _text += ';';
                 _inputText.setString(_text);
             } else if (key == sf::Keyboard::Comma && !_pressed) {
-                _text += ',';
-                _inputText.setString(_text);
-            } else if (key == sf::Keyboard::Dash && !_pressed) {
-                _text += '-';
-                _inputText.setString(_text);
-            } else if (key == sf::Keyboard::Equal && !_pressed) {
-                _text += '=';
-                _inputText.setString(_text);
-            } else if (key == sf::Keyboard::SemiColon && !_pressed) {
-                _text += ';';
+                if (shiftPressed)
+                    _text += '?';
+                else
+                    _text += ',';
                 _inputText.setString(_text);
             } else if (key == sf::Keyboard::Slash && !_pressed) {
-                _text += '/';
-                _inputText.setString(_text);
-            } else if (key == sf::Keyboard::BackSlash && !_pressed) {
-                _text += '\\';
-                _inputText.setString(_text);
-            } else if (key == sf::Keyboard::Quote && !_pressed) {
-                _text += '\'';
-                _inputText.setString(_text);
-            } else if (key == sf::Keyboard::LBracket && !_pressed) {
-                _text += '[';
-                _inputText.setString(_text);
-            } else if (key == sf::Keyboard::RBracket && !_pressed) {
-                _text += ']';
-                _inputText.setString(_text);
-            } else if (key == sf::Keyboard::Tilde && !_pressed) {
-                _text += '~';
-                _inputText.setString(_text);
-            } else if (key == sf::Keyboard::Add && !_pressed) {
-                _text += '+';
-                _inputText.setString(_text);
-            } else if (key == sf::Keyboard::Multiply && !_pressed) {
-                _text += '*';
-                _inputText.setString(_text);
-            } else if (key == sf::Keyboard::Subtract && !_pressed) {
-                _text += '-';
-                _inputText.setString(_text);
-            } else if (key == sf::Keyboard::Divide && !_pressed) {
-                _text += '/';
+                if (shiftPressed)
+                    _text += '/';
+                else
+                    _text += ':';
                 _inputText.setString(_text);
             } else if (key == sf::Keyboard::Space && !_pressed) {
                 _text += ' ';
