8b4a199433ad38df48fcb1721d520f6b776b488b
Léo Brossard <leo_brossard@hotmail.fr>
2023-03-05 23:20:19 +0100

    fix(translate): adding translator

--- include/KappaEngine/Managers/TranslateManager.hpp
+++ include/KappaEngine/Managers/TranslateManager.hpp
@@ -4,21 +4,37 @@
 
 #pragma once
 #include <yaml-cpp/yaml.h>
+#include <SFML/Graphics/Text.hpp>
 
 namespace KappaEngine {
     class TranslateManager {
         public:
             static void loadTranslates();
 
-            static void registerTranslate(std::string *translate, std::string key);
+            static void registerTranslate(std::string *translate, const std::string& key);
+            static void registerTranslate(sf::Text *translate, const std::string& key);
+
+            static void translate(std::string *translate, const std::string& key);
+            static void translate(sf::Text *translate, const std::string& key);
+
+            static void setTranslate(const std::string& translate);
+
+            static std::string getCurrentTranslate();
+            static std::string getDefaultTranslate();
+
+            static std::vector<std::string> getAvailableTranslates();
+
+            static std::string getTranslate(const std::string& key);
         private:
             static void updateTranslates();
 
             static YAML::Node _configs;
             static std::map<std::string, YAML::Node> _translateData;
             static std::vector<std::pair<std::string *, std::string>> _translatePointers;
+            static std::vector<std::pair<sf::Text *, std::string>> _translateTextPointers;
             static std::string _defaultTranslate;
             static std::string _currentTranslate;
+            static std::vector<std::string> _availableTranslates;
     };
 
 }
--- src/Managers/TranslateManager.cpp
+++ src/Managers/TranslateManager.cpp
@@ -2,16 +2,29 @@
 // Created by leo_b on 05/03/2023.
 //
 
+#include <filesystem>
+#include <iostream>
 #include "KappaEngine/Managers/TranslateManager.hpp"
 
 namespace KappaEngine {
+
+    YAML::Node TranslateManager::_configs;
+    std::map<std::string, YAML::Node> TranslateManager::_translateData;
+    std::vector<std::pair<std::string *, std::string>> TranslateManager::_translatePointers;
+    std::vector<std::pair<sf::Text *, std::string>> TranslateManager::_translateTextPointers;
+    std::string TranslateManager::_defaultTranslate;
+    std::string TranslateManager::_currentTranslate;
+    std::vector<std::string> TranslateManager::_availableTranslates;
+
     void TranslateManager::loadTranslates() {
         _translateData.clear();
 
-        _configs = YAML::LoadFile("configs/translation.yaml");
+        if (!std::filesystem::exists("configs/translation.yaml"))
+            throw std::runtime_error("Can't load translate base configs\n"
+                                     "File configs/translation.yaml doesn't exist. "
+                                     "Please create it or check if it's in the right place");
 
-        if (!_configs)
-            throw std::runtime_error("Can't load translate base configs");
+        _configs = YAML::LoadFile("configs/translation.yaml");
 
         std::string basePath = (_configs["folder"]) ? _configs["folder"].as<std::string>() : "translations";
 
@@ -27,32 +40,95 @@ namespace KappaEngine {
                     continue;
 
                 std::string path = basePath + "/" + entry["file"].as<std::string>();
+
+                if (!std::filesystem::exists(path)) {
+                    std::cerr << "Can't load translate file: " << path << "\n Locale " << entry["code"].as<std::string>() << " will be ignored" << std::endl;
+                    continue;
+                }
+
                 YAML::Node translate = YAML::LoadFile(path);
-
-                if (!translate)
-                    throw std::runtime_error("Can't load translate file: " + path);
-
                 _translateData[entry["code"].as<std::string>()] = translate;
+                _availableTranslates.push_back(entry["code"].as<std::string>());
+
+                std::cout << "Loaded locale " << entry["code"].as<std::string>() << " from " << path << std::endl;
             }
         }
-
         updateTranslates();
     }
 
-    void TranslateManager::registerTranslate(std::string *translate, std::string key) {
-        _translatePointers.push_back(std::make_pair(translate, key));
+    std::string TranslateManager::getTranslate(const std::string& key) {
+        YAML::Node node;
 
+        if (_translateData.find(_currentTranslate) != _translateData.end())
+            node = Clone(_translateData[_currentTranslate]);
+        else if (_translateData.find(_defaultTranslate) != _translateData.end())
+            node = Clone(_translateData[_defaultTranslate]);
+        else {
+            std::cerr << "Can't find any translate file for locale " << _currentTranslate << " or " << _defaultTranslate << std::endl;
+            return key;
+        }
 
+        size_t pos = 0;
+        std::string token;
+        std::string cpkey = key;
+        while ((pos = cpkey.find('.')) != std::string::npos) {
+            token = cpkey.substr(0, pos);
 
+            if (!(node)[token])
+                return key;
+            node = node[token];
+            cpkey.erase(0, pos + 1);
+        }
+
+        return (node[cpkey] && node[cpkey].IsScalar()) ? node[cpkey].as<std::string>() : key;
+    }
+
+    void TranslateManager::setTranslate(const std::string& translate) {
+
+        if (_translateData.find(translate) == _translateData.end())
+            return;
+
+        _currentTranslate = translate;
+        updateTranslates();
+    }
+
+    std::string TranslateManager::getCurrentTranslate() {
+        return _currentTranslate;
+    }
+
+    std::string TranslateManager::getDefaultTranslate() {
+        return _defaultTranslate;
+    }
+
+    std::vector<std::string> TranslateManager::getAvailableTranslates() {
+        return _availableTranslates;
+    }
+
+    void TranslateManager::registerTranslate(std::string *str, const std::string& key) {
+        _translatePointers.emplace_back(str, key);
+        translate(str, key);
+    }
+
+    void TranslateManager::registerTranslate(sf::Text *text, const std::string& key) {
+        _translateTextPointers.emplace_back(text, key);
+        translate(text, key);
     }
 
     void TranslateManager::updateTranslates() {
         for (auto &entry : _translatePointers) {
-            if (_translateData.find(_configs["default"].as<std::string>()) == _translateData.end())
-                throw std::runtime_error("Can't find default translate");
-
-            if (_translateData[_configs["default"].as<std::string>()][entry.second])
-                *entry.first = _translateData[_configs["default"].as<std::string>()][entry.second].as<std::string>();
+            translate(entry.first, entry.second);
         }
+
+        for (auto &entry : _translateTextPointers) {
+            translate(entry.first, entry.second);
+        }
+    }
+
+    void TranslateManager::translate(std::string *str, const std::string& key) {
+        str->assign(getTranslate(key));
+    }
+
+    void TranslateManager::translate(sf::Text *text, const std::string& key) {
+        text->setString(getTranslate(key));
     }
 }
